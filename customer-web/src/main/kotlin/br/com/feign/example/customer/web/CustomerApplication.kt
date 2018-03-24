8bd4eb446741bf46da22fb325aef53ffecd68be7
Ricardo Marques <ricardo.borges@zup.com.br>
2018-03-24 13:19:53 -0300

    module customer web

---
+++ customer-web/src/main/kotlin/br/com/feign/example/customer/web/CustomerApplication.kt
@@ -0,0 +1,36 @@
+package br.com.feign.example.customer.web
+
+import org.apache.logging.log4j.LogManager
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import java.net.InetAddress
+
+private val logger = LogManager.getLogger("br.com.feing.example.customer.web.CustomerApplication")
+
+fun main(args: Array<String>) {
+    val app = SpringApplication.run(ApplicationConfig::class.java, *args)
+
+    val applicationName = app.environment.getProperty("spring.application.name")
+    val contextPath = app.environment.getProperty("server.contextPath")
+    val port = app.environment.getProperty("server.port")
+    val hostAddress = InetAddress.getLocalHost().hostAddress
+
+    logger.info("""|
+                   |------------------------------------------------------------
+                   |Application '$applicationName' is running! Access URLs:
+                   |   Local:      http://127.0.0.1:$port$contextPath
+                   |   External:   http://$hostAddress:$port$contextPath
+                   |------------------------------------------------------------""".trimMargin())
+
+}
+
+
+@SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = ["br.com.feign.example.customer.web"])
+@EnableAutoConfiguration(exclude = [(LiquibaseAutoConfiguration::class)])
+open class ApplicationConfig
